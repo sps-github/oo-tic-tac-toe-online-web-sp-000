26b9f77a78173ff52bf615a4830abb131d9b3cb4
Sarah Peters <sarahpieters15@gmail.com>
2020-12-05 10:52:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,16 +57,17 @@ class TicTacToe
 
   def won?
     board_empty = @board.none? { |i| i == "X" || i = "O"}
-    if board_empty
-      false
-    else
-      WIN_COMBINATIONS.each do |combo|
-        if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-          return combo
-        end
-      end
-      return false
-    end
-  end
+   if board_empty
+     false
+   else
+     WIN_COMBINATIONS.each do |combo|
+       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+         return combo
+       end
+     end
+     return false
+ end
+end
+
 
 end
