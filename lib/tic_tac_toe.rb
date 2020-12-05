9a6f4e7fd6373caf2029e5a00743f8e95e17dfcc
Sarah Peters <sarahpieters15@gmail.com>
2020-12-05 10:55:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,4 +81,16 @@ def full?
   won? || draw? || full? ? true : false
 end
 
+def winner 
+    WIN_COMBINATIONS.detect do |combo| 
+          if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
+            return "X"
+          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+            return "O"
+          else 
+            nil
+          end
+    end
+  end
+
 end
